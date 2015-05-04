49e667e1841bc18d293a91c63a5d62d17d76cf62
Sophal Kim <sophal.kim.work@gmail.com>
2015-05-03 21:11:26 -0700

    Added game loop and paddle constructor

---
+++ scripts/template.js
@@ -0,0 +1,46 @@
+var animate = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function(callback) { window.setTimeout(callback, 1000/60)};
+
+var canvas = document.createElement('canvas');
+var width = 400;
+var height = 600;
+canvas.width = width;
+canvas.height = height;
+var context = canvas.getContext('2d');
+
+window.onload = function() {
+    document.body.appendChild(canvas);
+    animate(step);
+};
+
+var step = function() {
+    update();
+    render();
+    animate(step);
+};
+
+var update = function() {
+
+};
+
+var render = function() {
+    context.fillStyle = "#FF00FF";
+    context.fillRect(0, 0, width, height);
+};
+
+function Paddle(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.x_speed = 0;
+    this.y_speed = 0;
+}
+
+Paddle.prototype.render = function() {
+    context.fillStyle = "#0000FF";
+    context.fillRect(this.x, this.y, this.width, this.height);
+};
+
